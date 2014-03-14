1d3d7a8cdc6425b4f245f699bc92855cde9139ea
Cucen <muzeyyen.histiroglu@gmail.com>
2014-03-14 17:53:55 +0200

    esovtaj

---
+++ js/araclar.js
@@ -0,0 +1,65 @@
+{
+"Araclar":
+	{
+	"Tumu":
+	 [
+	   {
+		'id' : 1,
+		'marka' : 'ISUZU D-MAX CIFT KABIN 4x2', 
+		'yer' : 'ANKARA / MERKEZ',
+		'Belge_Turu' : 'CEKME BELGELI',
+		'Kullaným_Tarzi' : 'KAMYONET',
+		'Model_Yili' : '2010',
+		'KM' : '112762',
+		'Renk' : 'GRI',
+		'Yakit_Turu' : 'DIZEL',
+		'Vites_Turu' : 'MANUEL',
+	    'Plaka_No' : '06HB937',
+	    'image' : 'http://192.168.1.100:8080/esovtaj/img/1.jpg',
+	   },
+	   {
+			'id' : 2,
+			'marka' : 'OPEL ASTRA HB CLASSIC', 
+			'yer' : 'BURSA / MERKEZ',
+			'Belge_Turu' : 'CEKME BELGELI',
+			'Kullaným_Tarzi' : 'HUSUSI',
+			'Model_Yili' : '2011',
+			'KM' : '50000',
+			'Renk' : 'GRI',
+			'Yakit_Turu' : 'DIZEL',
+			'Vites_Turu' : 'MANUEL',
+		    'Plaka_No' : '06HB937',
+		    'image' : 'http://192.168.1.100:8080/esovtaj/img/2.jpg'
+	   },
+	   {
+			'id' : 3,
+			'marka' : 'AUDI A3 SPORTBACK 1.6', 
+			'yer' : 'ISTANBUL / MERKEZ',
+			'Belge_Turu' : 'CEKME BELGELI',
+			'Kullaným_Tarzi' : 'KAMYONET',
+			'Model_Yili' : '2012',
+			'KM' : '100000',
+			'Renk' : 'GRI',
+			'Yakit_Turu' : 'DIZEL',
+			'Vites_Turu' : 'OTOMATIK',
+		    'Plaka_No' : '06HB937',
+		    'image' : 'http://192.168.1.100:8080/esovtaj/img/3.jpg',
+	    },
+		{
+				'id' :4,
+				'marka' : 'VOLKSWAGEN POLO 1.4', 
+				'yer' : 'ADANA / MERKEZ',
+				'Belge_Turu' : 'CEKME BELGELI',
+				'Kullaným_Tarzi' : 'KAMYONET',
+				'Model_Yili' : '2000',
+				'KM' : '110000',
+				'Renk' : 'GRI',
+				'Yakit_Turu' : 'DIZEL',
+				'Vites_Turu' : 'MANUEL',
+			    'Plaka_No' : '06HB937',
+			    'image' : 'http://192.168.1.100:8080/esovtaj/img/4.jpg',
+	     }
+    ]
+}
+
+}
